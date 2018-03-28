c8106570822f4e2619dded27b6360545feb666d7
isenu <isenu@Mac-mini.local>
2018-03-28 17:43:58 +0800

    new project

---
+++ JYSDKDemo/JYSDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JYSDKDemo
+//
+//  Created by isenu on 2018/3/28.
+//  Copyright © 2018年 isenu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JYSDKDemo/JYSDKDemoTests/JYSDKDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  JYSDKDemoTests.m
+//  JYSDKDemoTests
+//
+//  Created by isenu on 2018/3/28.
+//  Copyright © 2018年 isenu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JYSDKDemoTests : XCTestCase
+
+@end
+
+@implementation JYSDKDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
