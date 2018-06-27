0e18d31ca3e21fd52ec4ee56e6067cc77956e087
jayant hoo <jayanthoo@jayantdeMacBook-Pro.local>
2018-06-27 18:05:03 +0800

    添加字体大小分类（利用runtime设置等比例字体大小）

---
+++ JYSDKDemo/JKCategorys/UIFont+JYScale.h
@@ -0,0 +1,13 @@
+//
+//  UIFont+JYScale.h
+//  JYSDKDemo
+//
+//  Created by jayant hoo on 2018/6/27.
+//  Copyright © 2018年 isenu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFont (JYScale)
+
+@end
---
+++ JYSDKDemo/JKCategorys/UIFont+JYScale.m
@@ -0,0 +1,13 @@
+//
+//  UIFont+JYScale.m
+//  JYSDKDemo
+//
+//  Created by jayant hoo on 2018/6/27.
+//  Copyright © 2018年 isenu. All rights reserved.
+//
+
+#import "UIFont+JYScale.h"
+
+@implementation UIFont (JYScale)
+
+@end
