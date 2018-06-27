9a725a177d2ce4434c20e79d62ec0433c80f64a9
jayant hoo <jayanthoo@jayantdeMacBook-Pro.local>
2018-06-27 16:52:24 +0800

    修改单例父类

---
+++ JYSDKDemo/JYTPHelper/JYDefaultHelper.h
@@ -0,0 +1,13 @@
+//
+//  JYDefaultHelper.h
+//  JYSDKDemo
+//
+//  Created by jayant hoo on 2018/6/27.
+//  Copyright © 2018年 isenu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JYDefaultHelper : NSObject
+
+@end
---
+++ JYSDKDemo/JYTPHelper/JYDefaultHelper.m
@@ -0,0 +1,13 @@
+//
+//  JYDefaultHelper.m
+//  JYSDKDemo
+//
+//  Created by jayant hoo on 2018/6/27.
+//  Copyright © 2018年 isenu. All rights reserved.
+//
+
+#import "JYDefaultHelper.h"
+
+@implementation JYDefaultHelper
+
+@end
